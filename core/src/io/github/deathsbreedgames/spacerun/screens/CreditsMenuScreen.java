8318ae3b559162aa8405fd3980832435a9dcae71
Deathsbreed <nicolas.ortega.froysa@gmail.com>
2014-09-01 11:46:45 -0500

    Added screens for Credits Menu and Ship Select.

---
+++ core/src/io/github/deathsbreedgames/spacerun/screens/CreditsMenuScreen.java
@@ -0,0 +1,30 @@
+package io.github.deathsbreedgames.spacerun.sceens;
+
+/**
+ * @author Nicolás A. Ortega
+ * @copyright DeathsbreedGames
+ * @license GNU Affero GPLv3
+ * @year 2014
+ * 
+ * Description: This class takes care of the Credits Menu for the game.
+ * 
+ */
+public class CreditsMenuScreen extends BaseScreen {
+	
+	// Constructor:
+	public CreditsMenuScreen() {
+		super("MainMenu");
+	}
+	
+	// Update:
+	@Override
+	public void render(float delta) {
+		super.render(delta);
+	}
+	
+	// Dispose:
+	@Override
+	public void dispose() {
+		
+	}
+}
---
+++ core/src/io/github/deathsbreedgames/spacerun/screens/ShipSelectScreen.java
@@ -0,0 +1,31 @@
+package io.github.deathsbreedgames.spacerun.screens;
+
+/**
+ * @author Nicolás A. Ortega
+ * @copyright DeathsbreedGames
+ * @license GNU Affero GPLv3
+ * @year 2014
+ * 
+ * Description: This class is where the player chooses which ship s/he
+ * wants to use.
+ * 
+ */
+public class ShipSelectScreen extends BaseScreen {
+	
+	// Constructor:
+	public ShipSelectScreen() {
+		super("Game");
+	}
+	
+	// Update:
+	@Override
+	public void render(float delta) {
+		super.render(float delta);
+	}
+	
+	// Dispose:
+	@Override
+	public void dispose() {
+		
+	}
+}
