450cf9720115c8f028cfdeb70b9a2620cebd7345
Nicolás A. Ortega <nicolas.ortega.froysa@gmail.com>
2014-09-01 01:34:56 -0500

    Added icon to desktop version.

--- desktop/src/io/github/deathsbreedgames/spacerun/desktop/DesktopLauncher.java
+++ desktop/src/io/github/deathsbreedgames/spacerun/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package io.github.deathsbreedgames.spacerun.desktop;
 
+import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import io.github.deathsbreedgames.spacerun.SpaceRun;
@@ -12,6 +13,7 @@ public class DesktopLauncher {
 		config.width = 320;
 		config.foregroundFPS = 45;
 		config.backgroundFPS = 20;
+		config.addIcon("gfx/space-run.png", Files.FileType.Internal);
 		new LwjglApplication(new SpaceRun(), config);
 	}
 }
