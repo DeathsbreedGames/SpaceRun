2cc4cf22ad30383f9a89369a0e3fe9757f2f9556
Deathsbreed <nicolas.ortega.froysa@gmail.com>
2014-09-04 19:00:45 -0500

    Using icon for desktop version.

--- core/src/io/github/deathsbreedgames/spacerun/screens/ShipSelectScreen.java
+++ core/src/io/github/deathsbreedgames/spacerun/screens/ShipSelectScreen.java
@@ -1,5 +1,10 @@
 package io.github.deathsbreedgames.spacerun.screens;
 
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import io.github.deathsbreedgames.spacerun.GlobalVars;
+
 /**
  * @author Nicolás A. Ortega
  * @copyright DeathsbreedGames
@@ -11,21 +16,34 @@ package io.github.deathsbreedgames.spacerun.screens;
  * 
  */
 public class ShipSelectScreen extends BaseScreen {
+	private OrthographicCamera camera;
+	private SpriteBatch batch;
 	
 	// Constructor:
 	public ShipSelectScreen() {
 		super("Game");
+		
+		camera = new OrthographicCamera(GlobalVars.width, GlobalVars.height);
+		camera.position.set(GlobalVars.width / 2, GlobalVars.height / 2, 0f);
+		camera.update();
+		batch = new SpriteBatch();
 	}
 	
 	// Update:
 	@Override
 	public void render(float delta) {
 		super.render(delta);
+		
+		camera.update();
+		batch.setProjectionMatrix(camera.combined);
+		batch.begin();
+		// Draw here
+		batch.end();
 	}
 	
 	// Dispose:
 	@Override
 	public void dispose() {
-		
+		batch.dispose();
 	}
 }
