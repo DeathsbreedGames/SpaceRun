8d88b6c38a4fed76da13c7495e3e0983319d2c27
Deathsbreed <nicolas.ortega.froysa@gmail.com>
2014-09-27 17:41:19 -0500

    Fixed I18N error.

--- core/src/io/github/deathsbreedgames/spacerun/screens/CreditsMenuScreen.java
+++ core/src/io/github/deathsbreedgames/spacerun/screens/CreditsMenuScreen.java
@@ -137,7 +137,7 @@ public class CreditsMenuScreen extends BaseScreen {
 		textFont.draw(batch, "Fork me on Github!", 10f, 130f);
 		
 		textFont.setColor(1f, 1f, 1f, 1f);
-		String version = "v0.5";
+		String version = "v0.6";
 		float versionX = GlobalVars.width - (textFont.getBounds(version).width + 5);
 		textFont.draw(batch, version, versionX, 465f);
 		batch.end();
